44421e9ad7f0865e2cdbbc6a9a1529e685b11d74
ruairi callaghan <rcallaghan@apexfintechsolutions.com>
2023-02-01 14:07:46 +0000

    comm

--- container/go/cmd/stamper/stamper.go
+++ container/go/cmd/stamper/stamper.go
@@ -20,19 +20,19 @@ func main() {
 	flag.Var(&stampInfoFile, "stamp-info-file", "The list of paths to the stamp info files used to substitute supported attribute when a python format placeholder is provivided in dst, e.g., {BUILD_USER}.")
 	flag.Parse()
 
-	stamper, err := compat.NewStamper(stampInfoFile)
-	if err != nil {
-		log.Fatalf("Failed to initialize the stamper: %v", err)
-	}
+	// stamper, err := compat.NewStamper(stampInfoFile)
+	// if err != nil {
+	// 	log.Fatalf("Failed to initialize the stamper: %v", err)
+	// }
 
-	dst := "gcr.io/{STABLE_PROJECT_ID}/examples/simple-deployment:{STABLE_COMMIT_SHA}"
-	stamped := stamper.Stamp(dst)
-	if stamped != dst {
-		log.Printf("Destination %s was resolved to %s after stamping.", dst, stamped)
-	}
+	// dst := "gcr.io/{STABLE_PROJECT_ID}/examples/simple-deployment:{STABLE_COMMIT_SHA}"
+	// stamped := stamper.Stamp(dst)
+	// if stamped != dst {
+	// 	log.Printf("Destination %s was resolved to %s after stamping.", dst, stamped)
+	// }
 
-	if err := ioutil.WriteFile(*dest, []byte(stamped), os.ModePerm); err != nil {
-		log.Fatalf("Error outputting digest file to %s: %v", *dest, err)
-	}
+	// if err := ioutil.WriteFile(*dest, []byte(stamped), os.ModePerm); err != nil {
+	// 	log.Fatalf("Error outputting digest file to %s: %v", *dest, err)
+	// }
 
 }
